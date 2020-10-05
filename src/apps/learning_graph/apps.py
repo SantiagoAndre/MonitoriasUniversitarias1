ca49fdeb8e322f2b063bdd6f17fc787c00f0fe8e
Santiago <saramirez@unicauca.edu.co>
2020-10-05 04:42:11 -0500

    Add signal before mutate LearningLine transform name into title and remove repeated spaces

--- src/apps/learning_graph/apps.py
+++ src/apps/learning_graph/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class LearningGraphConfig(AppConfig):
     name = 'apps.learning_graph'
+    def ready(self):
+        import apps.learning_graph.signals
--- src/apps/learning_graph/models.py
+++ src/apps/learning_graph/models.py
@@ -5,16 +5,19 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class LearningLine(models.Model):
     """Linea de aprendizaje"""
-
+    class Meta:
+        unique_together = ["name"]
     name = models.CharField(_("name"),max_length=100)
     description = models.CharField(_("description"),max_length=200)   
     def __str__(self):
         return self.name
 class Subject(models.Model):
     """Tema academico"""
-    name = models.CharField(_("name"),max_length=100)
-    description = models.CharField(_("description"),max_length=200)   
-    parent = models.ForeignKey('self',verbose_name=_("parent subject"),on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ["name"]
+    name = models.CharField(_("name"),max_length=100,blank=False,null=False)
+    description = models.CharField(_("description"),max_length=200,blank=False,null=False)   
+    parent = models.ForeignKey('self',verbose_name=_("parent subject"),on_delete=models.CASCADE,null=False)
     def __str__(self):
         return self.name
     
